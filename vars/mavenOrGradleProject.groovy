62f119422ad369ba818bcc08ddbf10cfccb32441
Karishma Gupta <105378376+karishma1521success@users.noreply.github.com>
2025-03-03 12:24:16 +0530

    Update mavenOrGradleProject.groovy

--- vars/mavenOrGradleProject.groovy
+++ vars/mavenOrGradleProject.groovy
@@ -7,15 +7,19 @@ def call() {
         javac -version
     '''
     steps.echo 'check Build Tool (Gradle or Maven....... )'
-    env.projectType = 'Maven'
+    projectType = 'Maven'
     if (steps.fileExists('build.gradle')) {
         projectType = 'Gradle'
         steps.echo 'Gradle Project Detected'
         steps.sh 'gradle -version'
-    }else if (fileExists('pom.xml')) {
+    }else if (steps.fileExists('pom.xml')) {
         // projectType = 'Maven'
         steps.echo 'Maven project detected'
     }else {
         steps.error 'No build file found! Not a valid Gradle or Maven project.'
     }
+
+    env.projectType = projectType // Explicitly set env.projectType
+
+    steps.echo "Project type within shared library: ${projectType}" // Verification
 }

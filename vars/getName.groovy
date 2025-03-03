656d5d810bafe5d58c4e1893389d5263c974028e
Karishma Gupta <105378376+karishma1521success@users.noreply.github.com>
2025-03-03 15:49:15 +0530

    Create getName.groovy

---
+++ vars/getName.groovy
@@ -0,0 +1,6 @@
+def call(String namePath) {
+    String warFileName = sh(script: "basename ${namePath}", returnStdout: true).trim()
+    steps.echo "New WAR file: ${warFileName}"
+    // env.NEW_WAR_FILE = warFileName
+    steps.echo "Value for warFileName -> $warFileName"
+}

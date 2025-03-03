ee33ab1a880dc6959e34197f0e68e1695777bb97
Karishma Gupta <105378376+karishma1521success@users.noreply.github.com>
2025-03-03 15:06:04 +0530

    Create build.groovy

---
+++ vars/build.groovy
@@ -0,0 +1,45 @@
+
+def call(String projectType, String prodName) {
+    def steps = this.steps
+    def env = this.env
+
+    String warPath = ''
+
+    if (projectType == 'Gradle') {
+        warPath = 'build/libs'
+        steps.echo 'Building Project Artifact using Gradle'
+        steps.withEnv(["GRADLE_OPTS=-Xmx4096m -Dorg.gradle.daemon=true -Dorg.gradle.jvmargs='-Xmx4096m'"]) {
+            steps.sh 'chmod +x gradlew'
+            steps.sh '''
+                ./gradlew assemble -Dorg.gradle.java.home="/usr/lib/jvm/java-11-openjdk"
+            '''
+        }
+    } else if (projectType == 'Maven') {
+        warPath = 'target'
+        steps.echo 'Building Project Artifact using Maven...'
+        steps.dir("${env.JENKINS_HOME}/workspace/${env.JOB_NAME}") {
+            steps.sh 'mvn clean package install -f pom.xml'
+        }
+    }
+
+    steps.sh """
+        warFile=\$(ls ${env.JENKINS_HOME}/workspace/${env.JOB_NAME}/${warPath}/*.war | head -n 1)
+        steps.echo "Source file: \$warFile"
+        steps.echo "Destination file: ${env.JENKINS_HOME}/workspace/${env.JOB_NAME}/${warPath}/${prodName}.war"
+
+        if [ "\$warFile" != "${env.JENKINS_HOME}/workspace/${env.JOB_NAME}/${warPath}/${prodName}.war" ]; then
+            mv \$warFile ${env.JENKINS_HOME}/workspace/${env.JOB_NAME}/${warPath}/${prodName}.war
+            steps.echo "File renamed successfully."
+        else
+            steps.echo "File name is perfect, no need to change"
+        fi
+
+        ls -lah ${env.JENKINS_HOME}/workspace/${env.JOB_NAME}/${warPath}/
+    """
+
+    this.getName("${env.JENKINS_HOME}/workspace/${env.JOB_NAME}/${warPath}/*.war")
+
+    String filename = steps.sh(script: "ls ${env.JENKINS_HOME}/workspace/${env.JOB_NAME}/${warPath}/${env.warFileName}", returnStdout: true).trim()
+    env.appName = steps.sh(script: """basename ${filename} | sed 's/##.*//'""", returnStdout: true).trim()
+    steps.echo "Application Name: ${env.appName}"
+}
